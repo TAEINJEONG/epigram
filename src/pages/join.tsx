3052380856a190a02bd55c14573b7eda184a0163
TAEINJEONG <taeinjeong46@gmail.com>
2025-06-13 17:58:20 +0900

    feat: react-hook-form 설치

--- src/pages/join.tsx
+++ src/pages/join.tsx
@@ -4,14 +4,13 @@ import { useState } from 'react';
 const Join = () => {
   const [value, setValue] = useState<string>('');
   return (
-    <div>
-      <div>어허</div>
+    <div className="p-20">
       <Input
         value={value}
         onChange={(e) => setValue(e.target.value)}
         type="password"
         placeholder="여기는 플레이스 홀더 입니다."
-        errorMessage="아니 글쌔 여기 에러가 있당께요?!"
+        errorMessage="에러 메세지"
       />
     </div>
   );
