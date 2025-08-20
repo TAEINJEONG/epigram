60a19360bb2b4f5fd08d1c2e12736abd55109bf1
TAEINJEONG <taeinjeong46@gmail.com>
2025-08-20 18:39:46 +0900

    fix: next-18n 경로 추가

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,14 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const path = require('path');
+
 module.exports = {
   i18n: {
     locales: ['ko', 'en'],
     defaultLocale: 'ko',
   },
   reloadOnPrerender: true,
-  localePath: './public/locales',
-  fallbackLng: {
-    default: ['ko'],
-  },
-  interpolation: {
-    escapeValue: false,
-  },
+  localePath: path.resolve('./public/locales'), // 절대 경로 사용
 };
